d01a68a08a21bee6b7ff42325a4d9905dc737c73
zxcabobus <llo113228@gmail.com>
2021-11-07 17:41:45 +0300

    master

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function SumbitAction(form){
-    var form = document.getElementById("itForm")
-    var accept = document.getElementById("accept")
-    accept.innerHTML = "Спасибо "+form.name.value+" за отправку заявки. Ожидайте."
+    var form = document.getElementById("itForm");
+    var accept = document.getElementById("accept");
+    accept.innerHTML = "Спасибо "+form.name.value+" за отправку заявки. Ожидайте.";
 }
