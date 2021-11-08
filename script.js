27e819703279a6de59084e0d1bcd70719d0e2e3d
zxcabobus <llo113228@gmail.com>
2021-11-08 12:07:18 +0300

    master

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
+function Valid(form){
+    var form = document.getElementById("itForm")
+    if(form.name.value==null || form.name.value=="")
+    {
+        alert("Введите имя")
+    }
+    else if(form.lastName.value==null || form.lastName.value=="")
+    {
+        alert("Введите фамилию")
+    }
+    else if(form.middleName.value==null || form.middleName.value=="")
+    {
+        alert("Введите отчество")
+    }
+    else if(form.mail.value==null || form.mail.value=="")
+    {
+        alert("Введите почту")
+    }
+}
+
 function SumbitAction(form){
-    var form = document.getElementById("itForm");
+    var form = document.getElementById("itForm")
     var accept = document.getElementById("accept");
     accept.innerHTML = "Спасибо "+form.name.value+" за отправку заявки. Ожидайте.";
 }
