8f7f11090f4fa341c56582523de8eefc5b0161a0
zxcabobus <llo113228@gmail.com>
2021-11-08 20:45:48 +0300

    master

--- script.js
+++ script.js
@@ -1,25 +1,46 @@
 function Valid(form){
+    var accept = document.getElementById("accept")
     var form = document.getElementById("itForm")
     if(form.name.value==null || form.name.value=="")
     {
+        accept.style.display = "none"
         alert("Введите имя")
     }
     else if(form.lastName.value==null || form.lastName.value=="")
     {
+        accept.style.display = "none"
         alert("Введите фамилию")
     }
     else if(form.middleName.value==null || form.middleName.value=="")
     {
+        accept.style.display = "none"
         alert("Введите отчество")
     }
     else if(form.mail.value==null || form.mail.value=="")
     {
+        accept.style.display = "none"
         alert("Введите почту")
+    }
+    else if(form.middleName.value!=null || form.middleName.value!="")
+    {
+        accept.style.display = "inline"
+    }
+    else if(form.lastName.value!=null || form.lastName.value!="")
+    {
+        accept.style.display = "inline"
+    }
+    else if(form.name.value!=null || form.name.value!="")
+    {
+        accept.style.display = "inline"
+    }
+    else if (form.mail.value!=null || form.mail.value!="")
+    {
+        accept.style.display = "inline"
     }
 }
 
 function SumbitAction(form){
-    var form = document.getElementById("itForm")
+    var form = document.getElementById("itForm");
     var accept = document.getElementById("accept");
-    accept.innerHTML = "Спасибо "+form.name.value+" за отправку заявки. Ожидайте.";
+    accept.innerHTML = "Спасибо. " + form.lastName.value + " " + form.name.value + " " + form.middleName.value + " " + "Ваша заявка принята. Вам отправленно писмо на почту " + form.mail.value + "."
 }
